4120b6c647818920c587683565c5bf94165b10c5
Dylan Beattie <dylan@dylanbeattie.net>
2020-12-01 16:18:19 +0000

    Naive but working solution to part 1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
-namespace day1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace day1 {
+	class Program {
+		static void Main(string[] args) {
+			var ints = File.ReadAllLines("input.txt")
+            .Select(s => Int32.Parse(s)).ToList();
+            for (int i = 0; i < ints.Count; i++) {
+                var first = ints[i];
+                for(int j = i+1; j < ints.Count; j++) {
+                    var second = ints[j];
+                    if (ints[i] + ints[j] == 2020) {
+                        Console.WriteLine(ints[i] * ints[j]);
+                        return;
+                    }
+                }
+            }
+		}
+	}
 }

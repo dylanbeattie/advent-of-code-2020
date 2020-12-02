8f1f5f28d915ab4736f6795e74fb0bcf676ac5ad
Dylan Beattie <dylan@dylanbeattie.net>
2020-12-02 16:10:39 +0000

    Nick's LINQ solution to Day 1

--- day1/Program.cs
+++ day1/Program.cs
@@ -6,33 +6,46 @@ using System.Linq;
 namespace day1 {
 	class Program {
 		static void Main(string[] args) {
-            try {
-			var ints = File.ReadAllLines("input.txt")
-				.Select(s => Int32.Parse(s)).ToList();
+			try {
+				var ints = File.ReadAllLines("input.txt")
+					.Select(s => Int32.Parse(s)).ToList();
 
-                var howManyIntsToSum = 3;
-                var requiredSum = 2020;
+				var howManyIntsToSum = 3;
+				var requiredSum = 2020;
 
-            var solution = Solve(ints, howManyIntsToSum, requiredSum);
-            Console.WriteLine(solution);
-            } catch(Exception ex) {
-                Console.Error.WriteLine(ex.Message);
-            }
+				var solution = Solve(ints, howManyIntsToSum, requiredSum);
+				Console.WriteLine(solution);
+			} catch (Exception ex) {
+				Console.Error.WriteLine(ex.Message);
+			}
 		}
 
-        // General-purpose recursive solution for finding the product of a set of integers
-        // in a list that add up to a given sum.
-        static int Solve(IEnumerable<int> ints, int howManyIntsToSum, int requiredSum) {
-            if (howManyIntsToSum == 0 || ! ints.Any()) return 0;
-            var head = ints.First();
-            var tail = ints.Skip(1);
-            if (howManyIntsToSum == 1 && head == requiredSum) return(head);
-            var result = head * Solve(tail, howManyIntsToSum - 1, requiredSum - head);
-            if (result != 0) return result;
-            return Solve(tail, howManyIntsToSum, requiredSum);
-        }
+		// Nick Gilbert's LINQ solution is quite lovely. :)
+		static int LinqSolution(IEnumerable<int> ints) {
+			return 
+			(from x in ints
+			 from y in ints.Skip(1)
+			 from z in ints.Skip(2)
+			 where x + y + z == 2020
+			 select x * y * z).FirstOrDefault();
+			 
+		}
 
-        // Naive solution using for() loops for part 2
+		// General-purpose recursive solution for finding the product of a set of integers
+		// in a list that add up to a given sum.
+		static int Solve(IEnumerable<int> ints, int howManyIntsToSum, int requiredSum) {
+			f
+
+			if (howManyIntsToSum == 0 || !ints.Any()) return 0;
+			var head = ints.First();
+			var tail = ints.Skip(1);
+			if (howManyIntsToSum == 1 && head == requiredSum) return (head);
+			var result = head * Solve(tail, howManyIntsToSum - 1, requiredSum - head);
+			if (result != 0) return result;
+			return Solve(tail, howManyIntsToSum, requiredSum);
+		}
+
+		// Naive solution using for() loops for part 2
 		static int SolvePart2(List<int> ints) {
 			for (int i = 0; i < ints.Count; i++) {
 				for (int j = i + 1; j < ints.Count; j++) {
@@ -46,7 +59,7 @@ namespace day1 {
 			return 0;
 		}
 
-        // Naive solution using for() loops for part 1
+		// Naive solution using for() loops for part 1
 		static int SolvePart1(List<int> ints) {
 			for (int i = 0; i < ints.Count; i++) {
 				for (int j = i + 1; j < ints.Count; j++) {

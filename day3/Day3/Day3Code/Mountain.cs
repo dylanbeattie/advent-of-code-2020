02a40e7a9cbfd657d563049a69709dd79ef3718e
Dylan Beattie <dylan@dylanbeattie.net>
2020-12-03 16:33:39 +0000

    Solved part 1 of day 3 in C#

---
+++ day3/Day3/Day3Code/Mountain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Day3Code {
+	public class Mountain {
+		public int[][] Cells { get; set;  }
+
+		public Mountain(string[] input) {
+			this.Cells = input
+				.Select(line => line.Select(c => c == '#' ? 1 : 0).ToArray()).ToArray();
+		}
+
+		private int Height => this.Cells.Length;
+		private int MapWidth => this.Cells[0].Length;
+
+
+		public int CountTrees(int dx, int dy) {
+			var treeCount = 0;
+			var x = 0;
+			for (var y = 0; y < this.Height; y+= dy) {
+				treeCount += this.Cells[y][x];
+				x = (x + dx) % this.MapWidth;
+			}
+			return (treeCount);
+		}
+	}
+}
---
+++ day3/Day3/Day3Code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Day3Code {
+	class Program {
+		static void Main(string[] args) {
+			var mountain = new Mountain(File.ReadAllLines("input.txt"));
+			var trees = mountain.CountTrees(3, 1);
+			Console.WriteLine(trees);
+		}
+	}
+}

4843c60940e17fca41672c772335846009df5663
Dylan Beattie <dylan@dylanbeattie.net>
2020-12-08 17:51:09 +0000

    Solutions to day 8 in C#

---
+++ day8/Day8/Day8Code/Computer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day8Code {
+	class Computer {
+		public List<Instruction> Instructions { get; set; }
+		private Dictionary<int, int> Counters { get; set; } = new Dictionary<int, int>();
+		public int Accumulator { get; set; }
+		public int Pointer { get; set; }
+
+		private Computer(List<Instruction> instructions) {
+			this.Instructions = instructions;
+		}
+
+		public Computer(string listing) {
+			var lines = listing
+				.Replace("\r", "").Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(line => line.Trim()).ToList();
+			this.Instructions = lines.Select(Parse).ToList();
+		}
+
+		public Computer Mutate(int instruction) {
+			var newInstructions = Instructions.Select(item => item).ToList();
+			newInstructions[instruction] = newInstructions[instruction].Mutate();
+			return new Computer(newInstructions);
+		}
+
+		public int Run() {
+			while (true) {
+				if (Pointer >= Instructions.Count) return 0;
+				var instr = Instructions[Pointer];
+				if (!Counters.ContainsKey(Pointer)) Counters.Add(Pointer, 0);
+				if (Counters[Pointer] > 0) return 1;
+				Counters[Pointer]++;
+				switch (instr) {
+					case Acc acc:
+						Accumulator += acc.Arg;
+						Pointer++;
+						break;
+					case Jmp jmp:
+						Pointer += jmp.Arg;
+						break;
+					case Nop nop:
+						Pointer++;
+						break;
+				}
+			}
+		}
+
+		private Instruction Parse(string line) {
+			var tokens = line.Split(" ");
+			var arg = Int32.Parse(tokens[1]);
+			return tokens[0] switch
+			{
+				"acc" => new Acc(arg),
+				"jmp" => new Jmp(arg),
+				"nop" => new Nop(arg),
+				_ => throw new NotImplementedException($"We cannot parse instructions with code {tokens[0]}")
+			};
+		}
+	}
+}
---
+++ day8/Day8/Day8Code/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Day8Code {
+	class Program {
+		private const string listing = @"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+		static void Main(string[] args) {
+			var prog = new Computer(File.ReadAllText("input.txt"));
+			for (int victim = 0; victim < prog.Instructions.Count; victim++) {
+				var mutation = prog.Mutate(victim);
+				var mutatedResult = mutation.Run();
+				if (mutatedResult == 0) {
+					Console.WriteLine($"YOLO! when mutating instruction {victim}");
+					Console.WriteLine(mutation.Accumulator);
+				}
+			}
+		}
+	}
+
+	abstract class Instruction {
+		protected Instruction(int arg) {
+			Arg = arg;
+		}
+		public int Arg { get; set; }
+
+		public virtual Instruction Mutate() {
+			return this;
+		}
+	}
+
+	class Acc : Instruction {
+		public Acc(int arg) : base(arg) { }
+	}
+
+	class Jmp : Instruction {
+		public Jmp(int arg) : base(arg) { }
+		public override Instruction Mutate() {
+			return new Nop(Arg);
+		}
+	}
+
+	class Nop : Instruction {
+		public Nop(int arg) : base(arg) { }
+
+		public override Instruction Mutate() {
+			return new Jmp(Arg);
+		}
+
+	}
+}

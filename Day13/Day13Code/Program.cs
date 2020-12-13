0e90ef11fefca5b29474352dfb5d1bca1f29b528
Dylan Beattie <dylan@dylanbeattie.net>
2020-12-13 20:44:05 +0000

    Solved it in 30 ms using recursion and prime things.

--- Day13/Day13Code/Program.cs
+++ Day13/Day13Code/Program.cs
@@ -10,34 +10,47 @@ namespace Day13Code {
 	 */
 
 	class Program {
-		//private static readonly int[] input = new[] {
-		//	19, 0, 0, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		//	743, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 17, 0, 0, 0, 0,
-		//	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 29, 0, 643, 0, 0, 0, 0, 0, 37,
-		//	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23
-		//};
+		// private static readonly int[] input = new[] { 7, 13, 0, 0, 59, 0, 31, 19 };
+		// Should yield 1068781
 
-		private static readonly int[] input = new[] { 7, 13, 0, 0, 59, 0, 31 };
+		private static readonly int[] input = new[] {
+			19, 0, 0, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			743, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 17, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 29, 0, 643, 0, 0, 0, 0, 0, 37,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23
+		};
 
 		static void Main(string[] args) {
 			var sw = new Stopwatch();
 			sw.Start();
-			(var root, var delta) = FindInterval(input, 5);
-			Console.WriteLine(root);
-			Console.WriteLine(delta);
-			Console.ReadKey(false);
-		}
-
-		static (long, long) FindInterval(int[] input, int limit) {
+			var limit = 2;
 			long initial = 0;
 			long interval = 1;
-			for (long i = 0; i < Int64.MaxValue; i++) {
+			while (limit < input.Length) {
+				(initial, interval) = FindInterval(initial, interval, input, limit++);
+			}
+			var ts = initial;
+			while (true) {
+				if (SolvesPart2(ts)) {
+					Console.WriteLine(ts);
+					sw.Stop();
+					Console.WriteLine($"Solved in {sw.ElapsedMilliseconds}ms");
+					break;
+				}
+				ts += interval;
+			}
+		}
+
+		static (long, long) FindInterval(long initial, long interval, int[] input, int limit) {
+			var stillLooking = true;
+			for (long i = initial; i < Int64.MaxValue; i+= interval) {
 				if (!input.Take(limit).Select((busId, index) => {
 					if (busId == 0) return (true);
 					return (i + index) % busId == 0;
 				}).All(b => b)) continue;
-				if (initial == 0) {
+				if (stillLooking) {
 					initial = i;
+					stillLooking = false;
 				} else {
 					interval = i - initial;
 					break;
@@ -48,12 +61,6 @@ namespace Day13Code {
 
 
 		static bool SolvesPart2(long ts) {
-
-			//var things = input.Select((busId, index) => (busId, index))
-			//	.ToDictionary(tuple => tuple.busId, tuple => tuple.index);
-
-			//Enumerable.Range(0, Int32.MaxValue)
-			//	.Select(i => )
 			var checks = input.Select((busId, index) => {
 				if (busId == 0) return (true);
 				return (ts + index) % busId == 0;

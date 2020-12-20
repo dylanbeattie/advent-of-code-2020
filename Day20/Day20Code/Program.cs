11d8cb5f3ddf96415cf11d8fc79a1114cbf6a282
Dylan Beattie <dylan@dylanbeattie.net>
2020-12-20 17:41:13 +0000

    Finished refactoring for part 2

--- Day20/Day20Code/Program.cs
+++ Day20/Day20Code/Program.cs
@@ -4,13 +4,14 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Day20Code {
 	class Program {
 		static void Main(string[] args) {
 			var input = File.ReadAllText("input.txt").Replace("\r", "")
 				.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
-			var tiles = input.SelectMany(Tile.Parse).ToList();
+			var tiles = input.Select(block => new Tile(block)).ToList();
 			var tileIds = new Dictionary<int, List<long>>();
 			foreach (var tile in tiles) {
 				foreach (var edge in tile.AllEdges) {
@@ -32,27 +33,21 @@ namespace Day20Code {
 		}
 	}
 
-	class Tile {
-		public long Id { get; set; }
+	class FlippedTile {
+
 		public int Top { get; set; }
 		public int Bottom { get; set; }
 		public int Left { get; set; }
 		public int Right { get; set; }
+		public Flip Flip { get; set; }
 
-		public static IEnumerable<Tile> Parse(string input) {
-			var lines = input.Split("\n");
-			var id = Int64.Parse(lines.First().Replace("Tile ", "").Replace(":", ""));
-			var rows = lines.Skip(1).Select(s => s.Replace(".", "0").Replace("#", "1")).ToList();
-			var t = rows.First();
-			var b = rows.Last();
-			var l = new String(rows.Select(row => row[0]).ToArray());
-			var r = new String(rows.Select(row => row[^1]).ToArray());
-			yield return new Tile(id, t, r, b, l);
-			yield return new Tile(id, t, r.Flip(), b, l.Flip());
-			yield return new Tile(id, t.Flip(), r.Flip(), b.Flip(), l.Flip());
-			yield return new Tile(id, t.Flip(), r, b.Flip(), l);
+		public FlippedTile(string[] rows, Flip flip) {
+			this.Flip = flip;
+			this.Top = Convert.ToInt32(rows.First(), 2);
+			this.Right = Convert.ToInt32(new String(rows.Select(row => row[^1]).ToArray()), 2);
+			this.Bottom = Convert.ToInt32(rows.Last(), 2);
+			this.Left = Convert.ToInt32(new String(rows.Select(row => row[0]).ToArray()), 2);
 		}
-
 		public IEnumerable<int> AllEdges {
 			get {
 				yield return Top;
@@ -61,18 +56,43 @@ namespace Day20Code {
 				yield return Right;
 			}
 		}
+		public override string ToString() {
+			return ($"{Top} {Right} {Bottom} {Left} ({Flip})");
+		}
+	}
 
-		private Tile(long id, string top, string right, string bottom, string left) {
-			this.Id = id;
-			this.Top = Convert.ToInt32(top, 2);
-			this.Right = Convert.ToInt32(right, 2);
-			this.Bottom = Convert.ToInt32(bottom, 2);
-			this.Left = Convert.ToInt32(left, 2);
+	class Tile {
+		public long Id { get; set; }
+		public Dictionary<Flip, FlippedTile> Flips { get; set; } = new Dictionary<Flip, FlippedTile>();
+
+		public Tile(string input) {
+			var lines = input.Split("\n");
+			this.Id = Int64.Parse(lines.First().Replace("Tile ", "").Replace(":", ""));
+			var rows = lines.Skip(1).Select(s => s.Replace(".", "0").Replace("#", "1")).ToArray();
+			this.Flips.Add(Flip.None, new FlippedTile(rows, Flip.None));
+			this.Flips.Add(Flip.TopBottom, new FlippedTile(rows.Reverse().ToArray(), Flip.TopBottom));
+			this.Flips.Add(Flip.LeftRight, new FlippedTile(rows.Select(r => r.Flip()).ToArray(), Flip.LeftRight));
+			this.Flips.Add(Flip.Both, new FlippedTile(rows.Select(r => r.Flip()).Reverse().ToArray(), Flip.Both));
+		}
+
+		public IEnumerable<int> AllEdges {
+			get { return Flips.SelectMany(kvp => kvp.Value.AllEdges); }
 		}
 
 		public override string ToString() {
-			return ($"ID {Id} : {Top} {Right} {Bottom} {Left}");
+			return $"{Id}:" +
+				   $"  None: {this.Flips[Flip.None]}" +
+				   $"  ToBo: {this.Flips[Flip.TopBottom]}" +
+				   $"  LeRi: {this.Flips[Flip.LeftRight]}" +
+				   $"  Both: {this.Flips[Flip.Both]}";
 		}
+	}
+
+	public enum Flip {
+		None,
+		LeftRight,
+		TopBottom,
+		Both
 	}
 
 	public static class StringExtensions {
